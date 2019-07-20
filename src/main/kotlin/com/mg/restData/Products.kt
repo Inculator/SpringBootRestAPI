b4be8e8a5c279be4bb25175028a04b921b44f4e0
MOHAK GUPTA <mohakgupta.2012@gmail.com>
2019-07-20 22:03:37 +0530

    Adding test case

--- src/main/kotlin/com/mg/restData/Products.kt
+++ src/main/kotlin/com/mg/restData/Products.kt
@@ -3,6 +3,7 @@ package com.mg.restData
 object Products {
         var myList: ArrayList<Product> = ArrayList()
         fun makeList() {
+            myList.clear()
             myList.add(Product(1,"Khadi Hair cleanser", 120))
             myList.add(Product(2,"Khadi Hair serun", 198))
             myList.add(Product(3,"Khadi Rosewater", 130))
---
+++ src/test/kotlin/TestRestHeaders.kt
@@ -0,0 +1,35 @@
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
+import org.apache.tomcat.jni.Lock.name
+import java.net.HttpURLConnection
+import java.net.URL
+import jdk.nashorn.internal.runtime.ScriptingFunctions.readLine
+import org.junit.Assert
+import org.junit.Test
+import org.springframework.boot.test.context.TestComponent
+import java.io.InputStreamReader
+import java.io.BufferedReader
+
+
+
+class TestRestHeaders {
+
+    @Test
+    fun testMyRestAPI(){
+        var url: URL = URL("http://localhost:8080/getProducts/3");
+        var conn =  url.openConnection()
+        conn.setRequestProperty("method", "POST")
+        conn.setRequestProperty("Accept", "application/json")
+        conn.setRequestProperty("myHeader", "ABC")
+
+        val br = BufferedReader(
+            InputStreamReader(
+                conn.getInputStream()
+            )
+        )
+
+        println("Output from Server .... \n")
+        var list = br.readLines()
+        Assert.assertEquals("{\"productId\":3,\"productName\":\"Khadi Rosewater\",\"productPrice\":130}",
+            list.get(0))
+    }
+}
